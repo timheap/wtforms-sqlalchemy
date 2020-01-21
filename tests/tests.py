6a069bcae2fec5443862400810b62a2d8a0c7f33
Tim Heap <tim.heap@tidetech.org>
2020-01-22 10:39:24 +1100

    Show blank option in QuerySelectField if nothing is selected
    Otherwise the first option is effectively the default. If users don't
    change anything, or don't notice the select, then the first option will
    be automatically selected.

--- tests/tests.py
+++ tests/tests.py
@@ -105,7 +105,7 @@ class QuerySelectFieldTest(TestBase):
             .filter(self.Test.id == 1, self.Test.id != 1)
             .all()
         )
-        self.assertEqual(form.a(), [])
+        self.assertEqual(form.a(), [('__None', '', True)])
 
 
     def test_with_query_factory(self):
@@ -118,7 +118,7 @@ class QuerySelectFieldTest(TestBase):
 
         form = F()
         self.assertEqual(form.a.data, None)
-        self.assertEqual(form.a(), [('1', 'apple', False), ('2', 'banana', False)])
+        self.assertEqual(form.a(), [('__None', '', True), ('1', 'apple', False), ('2', 'banana', False)])
         self.assertEqual(form.b.data, None)
         self.assertEqual(form.b(), [('__None', '', True), ('hello1', 'apple', False), ('hello2', 'banana', False)])
         self.assertFalse(form.validate())
@@ -152,7 +152,7 @@ class QuerySelectFieldTest(TestBase):
             .filter(self.Test.id == 1, self.Test.id != 1)
             .all()
         )
-        self.assertEqual(form.a(), [])
+        self.assertEqual(form.a(), [('__None', '', True)])
 
 
 class QuerySelectMultipleFieldTest(TestBase):
